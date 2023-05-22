a442c2ebab88f4a4955f8b7f17d65c617cd773aa
Arthur Joppart <arthur@digita.ai>
2023-05-22 21:44:53 +0200

    feat: support more language formats

--- backend/src/models/Language.ts
+++ backend/src/models/Language.ts
@@ -1,10 +1,17 @@
 export type Language = 'en' | 'nl' | '-';
 
 export const parseLanguage = (lang: string): Language => {
-  switch (lang) {
+  switch (lang.trim().toLowerCase()) {
+    case 'engels':
+    case 'english':
     case 'en':
       return 'en';
+    case 'vlaams':
+    case 'be':
+    case 'nederlands':
     case 'nl':
+    case 'dutch':
+    case 'flemish':
       return 'nl';
     default:
       return '-';
