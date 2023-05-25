c1f5b81b702a81f45ff951a333184888a3cd48db
Arthur Joppart <arthur@digita.ai>
2023-05-25 20:20:34 +0200

    feat: use custom error message with class-validator

--- backend/src/models/SearchTermInput.ts
+++ backend/src/models/SearchTermInput.ts
@@ -2,6 +2,8 @@ import { IsString, MinLength } from 'class-validator';
 
 export class SearchTermInput {
   @IsString()
-  @MinLength(2)
+  @MinLength(2, {
+    message: 'searchTerm needs to be at least 2 characters long ma boi',
+  })
   searchTerm: string;
 }
