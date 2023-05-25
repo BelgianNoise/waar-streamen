b4cf443b528d00a82e57de9bd0036c54f99c6feb
Arthur Joppart <arthur@digita.ai>
2023-05-25 20:14:38 +0200

    chore: add all env variables to GET / output

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -10,6 +10,14 @@ export class AppService {
         ENABLE_CACHE: process.env.ENABLE_CACHE,
         CACHE_RETENTION_MINUTES: process.env.CACHE_RETENTION_MINUTES,
         EXPOSE_OPENAPI: process.env.EXPOSE_OPENAPI,
+
+        AUTH_STREAMZ_ID_TOKEN: `...${process.env.AUTH_STREAMZ_ID_TOKEN?.slice(-5)}`,
+        AUTH_STREAMZ_AUTH_TOKEN: `...${process.env.AUTH_STREAMZ_AUTH_TOKEN?.slice(-5)}`,
+        AUTH_STREAMZ_ACCESS_TOKEN: `...${process.env.AUTH_STREAMZ_ACCESS_TOKEN?.slice(-5)}`,
+        AUTH_STREAMZ_REFRESH_TOKEN: `...${process.env.AUTH_STREAMZ_REFRESH_TOKEN?.slice(-5)}`,
+        AUTH_STREAMZ_PROFILE: `...${process.env.AUTH_STREAMZ_PROFILE?.slice(-5)}`,
+
+        AUTH_VTMGO_AUTH_ID: `...${process.env.AUTH_VTMGO_AUTH_ID?.slice(-5)}`,
       },
     };
   }
