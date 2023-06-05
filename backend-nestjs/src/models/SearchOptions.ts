18fb031681b596f55c75a6b0ba70f2af66cce579
Arthur Joppart <arthur@digita.ai>
2023-06-05 21:56:07 +0200

    fix: linting error only in CI ?

--- backend-nestjs/src/models/SearchOptions.ts
+++ backend-nestjs/src/models/SearchOptions.ts
@@ -1,6 +1,7 @@
 // How resource intensive the data should be fetched.
 // Some data requires multiple fetches to be correctly retrieved.
 export const fetchDepths = ['shallow', 'deep', 'full'] as const;
+// eslint-disable-next-line prettier/prettier
 export type FetchDepth = typeof fetchDepths[number];
 
 export interface SearchOptions {
